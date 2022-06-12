d3929be8f4876815d9a17679578d3c7e52cc65d8
Pawel Flis <pawelekflis5@gmail.com>
2022-06-12 12:21:02 +0200

    DB dump

--- db/grotnet.sql
+++ db/grotnet.sql
@@ -1,28 +1,26 @@
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: grotnet
--- ------------------------------------------------------
--- Server version	5.7.33
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Wersja serwera:               5.7.33 - MySQL Community Server (GPL)
+-- Serwer OS:                    Win64
+-- HeidiSQL Wersja:              11.2.0.6213
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `brief`
---
 
+-- Zrzut struktury bazy danych grotnet
+DROP DATABASE IF EXISTS `grotnet`;
+CREATE DATABASE IF NOT EXISTS `grotnet` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
+USE `grotnet`;
+
+-- Zrzut struktury tabela grotnet.brief
 DROP TABLE IF EXISTS `brief`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `brief` (
+CREATE TABLE IF NOT EXISTS `brief` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `userAssigned` int(255) DEFAULT NULL,
   `tytul` varchar(255) DEFAULT NULL,
@@ -37,49 +35,22 @@ CREATE TABLE `brief` (
   CONSTRAINT `brief_ibfk_1` FOREIGN KEY (`status`) REFERENCES `statusbrief` (`id`),
   CONSTRAINT `brief_ibfk_2` FOREIGN KEY (`userAssigned`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `brief`
---
+-- Eksport danych został odznaczony.
 
-LOCK TABLES `brief` WRITE;
-/*!40000 ALTER TABLE `brief` DISABLE KEYS */;
-INSERT INTO `brief` VALUES (1,NULL,'asfasf',' asfasf',NULL,NULL);
-/*!40000 ALTER TABLE `brief` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `statusbrief`
---
-
+-- Zrzut struktury tabela grotnet.statusbrief
 DROP TABLE IF EXISTS `statusbrief`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `statusbrief` (
+CREATE TABLE IF NOT EXISTS `statusbrief` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nazwa` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `statusbrief`
---
+-- Eksport danych został odznaczony.
 
-LOCK TABLES `statusbrief` WRITE;
-/*!40000 ALTER TABLE `statusbrief` DISABLE KEYS */;
-/*!40000 ALTER TABLE `statusbrief` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
+-- Zrzut struktury tabela grotnet.user
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `haslo` varchar(255) NOT NULL,
@@ -91,50 +62,21 @@ CREATE TABLE `user` (
   KEY `typUsera` (`typUsera`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`typUsera`) REFERENCES `usertype` (`id`),
   CONSTRAINT `user_ibfk_2` FOREIGN KEY (`typUsera`) REFERENCES `usertype` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `user`
---
+-- Eksport danych został odznaczony.
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin@admin.com','21232f297a57a5a743894a0e4a801fc3','Weronika','Grotek',1),(2,'test@test.com','098f6bcd4621d373cade4e832627b4f6','Test','Test',2);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usertype`
---
-
+-- Zrzut struktury tabela grotnet.usertype
 DROP TABLE IF EXISTS `usertype`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usertype` (
+CREATE TABLE IF NOT EXISTS `usertype` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nazwa` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `usertype`
---
+-- Eksport danych został odznaczony.
 
-LOCK TABLES `usertype` WRITE;
-/*!40000 ALTER TABLE `usertype` DISABLE KEYS */;
-INSERT INTO `usertype` VALUES (1,'admin'),(2,'pracownik'),(3,'klient');
-/*!40000 ALTER TABLE `usertype` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-06-09  1:33:09
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
